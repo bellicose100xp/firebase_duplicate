2d79551e1bc3f73716a6465949556a90bba567a5
bellicose100xp <chandan7@icloud.com>
2014-12-20 15:35:09 -0800

    work_commit

--- app/app.js
+++ app/app.js
@@ -10,6 +10,46 @@
             $scope.product.style = '';
             //$scope.isDuplicate = "";
 
+            // options for category
+            $scope.categoryOptions = [
+                {
+                    'Id': 1,
+                    'name': 'Orange',
+                    'color': 'orange',
+                    'retail': 66.66
+                }
+                , {
+                    'Id': 2,
+                    'name': 'Green',
+                    'color': 'green',
+                    'retail': 99.99
+                },
+                {
+                    'Id': 3,
+                    'name': 'Red',
+                    'color': 'red',
+                    'retail': 133.33
+                },
+                {
+                    'Id': 4,
+                    'name': 'Purple',
+                    'color': 'purple',
+                    'retail': 166.66
+                },
+                {
+                    'Id': 5,
+                    'name': 'Gold',
+                    'color': 'gold',
+                    'retail': 199.99
+                },
+                {
+                    'Id': 6,
+                    'name': 'Yellow',
+                    'color': '#ffff00',
+                    'retail': 249.99
+                }
+            ];
+
             $scope.addStyle = function (isValid) {
 
                 if (isValid) {
