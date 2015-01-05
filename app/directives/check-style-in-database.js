6bd7683431417c6e3a04da259342cd14b1066869
bellicose <bellicose.agnostic@gmail.com>
2015-01-05 09:36:13 -0800

    spare

--- app/directives/check-style-in-database.js
+++ app/directives/check-style-in-database.js
@@ -11,7 +11,7 @@ angular
         return {
             require: 'ngModel',
             scope: {},
-            link: function (scope, element, attrs, ngModel) {
+            link: function (scope, element, _, ngModel) {
 
                 //element.on('blur', function () {
                 //    var duplicate = myFactory.isDuplicate(ngModel.$viewValue);
@@ -21,17 +21,25 @@ angular
                 //});
 
                 // for ngModel $viewChangeListeners can be used instead of $watch by pushing a function
-                element.on('blur', function () {
-
+                //scope.$watch('model', function () {
+                //    console.log("change detected");
+                    element.on('blur', function () {
+                        console.log("View Value: " + ngModel.$viewValue);
+                        if (ngModel.$viewValue) {
+                            console.log("testing the value: " + ngModel.$viewValue);
                             var present = myFactory.isDuplicate(ngModel.$viewValue);
+                            console.log("present value: " + present);
                             ngModel.$setValidity('notPresentInDatabase', present);
 
-                            // change validity after certain time to hide the validity message
+                            //change validity after certain time to hide the validity message
                             $timeout(function () {
                                 ngModel.$setValidity('notPresentInDatabase', true);
-                            },5000);
+                            }, 3000);
+                        }
+                    });
 
-                });
+                //});
+
 
             }
         }
